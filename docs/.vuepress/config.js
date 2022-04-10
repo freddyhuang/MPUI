3cc45ee97f2a732b4fd79d9347c9f6a6ab7b6a67
freddyhuang <1358627726@qq.com>
2022-04-10 21:02:44 +0800

    feat:finish

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,18 @@
 module.exports = {
+    title:"MPUI",
     base:"/MPUI/",
     themeConfig: {
+      nav:[
+        {
+          text:"Home",link:"/"
+        },
+        {
+          text:"github",link:"https://github.com/freddyhuang/MPUI",
+        },
+        {
+          text:"Npm",link:"https://www.npmjs.com/package/mp-fast-ui",
+        }
+      ],
       sidebar: [
         '/',
         '/componentDocs/card'
